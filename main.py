649af56c3147182cca8114f5d465f8ec27117781
Ximo <ximo.dev.94@gmail.com>
2025-01-02 12:30:23 +0100

    add player rotation

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        player.update(dt)
+
         screen.fill(pygame.Color(0,0,0))
         player.draw(screen)
 
